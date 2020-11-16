9026cb03c07a0b50a1d11aa619e34e07e3de8d63
jonanOribe <jonan.oribe@gmail.com>
2020-11-16 19:23:54 +0100

    Update

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/gocolly/colly/v2 v2.1.0
 	github.com/gofiber/fiber/v2 v2.2.0
 	github.com/klauspost/compress v1.11.3 // indirect
 	golang.org/x/sys v0.0.0-20201116161645-c061ba923fbb // indirect
--- information_retriever.go
+++ information_retriever.go
@@ -1,13 +1,45 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/csv"
+	"log"
+	"os"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-    app := fiber.New()
+	fName := "cryptocoinmarketcap.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	// Write CSV header
+	writer.Write([]string{"Name", "Symbol","Market Cap", "Price (USD)", "Circulating Supply", "Volume (24h)	", "Change (1h)", "Change (24h)", "Change (7d)"})
 
-    app.Listen(":3000")
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	c.OnHTML(".cmc-table__table-wrapper-outer tbody tr", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText(".cmc-table__column-name"),
+			e.ChildText(".cmc-table__cell--sort-by__symbol"),
+			e.ChildText(".Text-sc-1eb5slv-0"), //Market Cap	
+			e.ChildText(".price___3rj7O "),
+			e.ChildText(".cmc-table__cell--sort-by__circulating-supply"),
+			e.ChildText(".cmc-table__cell--sort-by__volume-24-h"),
+			e.ChildText(".cmc-table__cell--sort-by__percent-change-1-h"),
+			e.ChildText(".cmc-table__cell--sort-by__percent-change-24-h"),
+			e.ChildText(".cmc-table__cell--sort-by__percent-change-7-d"),
+		})
+	})
+
+	c.Visit("https://coinmarketcap.com/all/views/all/")
+
+	log.Printf("Scraping finished, check file %q for results\n", fName)
 }
